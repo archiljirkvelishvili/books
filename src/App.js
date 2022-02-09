a62c7e48824e53fb4fd8b860932f3a265789203a
Archil Jirkvelishvili <archil.j@gmail.com>
2022-02-09 18:34:44 +0400

    project start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-
-import React  from "react"
-
-
+import React from 'react'
 
 export default function App() {
+  const [data, setData] = React.useState(null)
 
+  React.useEffect(() => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyBg9S1VTuUbjAV7wYjFoZeJaDnvFJWxvBw`)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setData(data)
+      })
+  }, [])
 
-  return(
-    <div><p>Hello</p></div>
+  return (
+    <div>
+      <p>{JSON.stringify(data)}</p>
+      <p>hello</p>
+    </div>
   )
 }
-
-
