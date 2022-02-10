0c86e4e74bba06a4211c075b9f04eec66684eaa8
Archil Jirkvelishvili <archil.j@gmail.com>
2022-02-10 19:11:44 +0400

    review

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react'
+
+import { Nav } from './components/Nav'
 import { Pages } from './pages/index'
+
 export function App() {
-  return <Pages />
+  return (
+    <div>
+      <Nav />
+      <Pages />
+    </div>
+  )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import { About } from './about'
-import { Blog } from './blog'
-import { Home } from './home'
+
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './home/home'
 
 export function Pages() {
   return (
-    <div>
-      <nav>
-        <Link to="/"> Home</Link>
-        <Link to="/blog"> Blog </Link>
-        <Link to="/about"> About</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
   )
 }
