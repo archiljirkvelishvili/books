ba3f8b86c7e5d3ff2fe33d4a76464ead5e2b5f33
Archil Jirkvelishvili <archil.j@gmail.com>
2022-02-23 03:05:34 +0400

    problem with fetch and abortcontroller need help!

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,26 +1,48 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
+
+// const initState = {
+//   error: null,
+//   loading: false,
+// }
+
+// const reducer = (state, action) => {
+//   // eslint-disable-next-line default-case
+//   switch (action.type) {
+//     case 'loading':
+//       return {...state, loading: true}
+//   }
+// }
 
 export function useFetch(url) {
+  // const [state, dispach] = useReducer(reducer, initState)
+
   const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState(null)
 
   useEffect(() => {
-    setLoading(true)
-    fetch(url)
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
+    console.log('ha')
+    let controller = new AbortController()
+    const { signal } = controller
+    console.log(controller)
+    ;(async () => {
+      try {
+        const response = await fetch(url, { signal })
+        const data = await response.json()
+        console.log(data.items)
         setData(data.items)
-      })
-      .catch(error => {
-        setError(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
+      } catch (error) {
+        console.log(error.message)
+        throw error
+      }
+    })()
+
+    return () => {
+      controller.abort()
+    }
   }, [url])
 
-  return { data, loading, error }
+  console.log(2)
+
+  return { data }
 }
