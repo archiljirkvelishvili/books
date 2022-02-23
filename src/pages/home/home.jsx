25ef545c01fa5d8334b160c7c6803ac345f4c0ba
Archil Jirkvelishvili <archil.j@gmail.com>
2022-02-24 02:49:44 +0400

    feat: adding booklist component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
+import { Booklist } from '../../components/Booklist'
+import { useFetch } from '../../hooks/useFetch'
 import './home.css'
 
 export function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+  const { data } = useFetch('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDNf5oC5ii63zrqDUB0w_F5OSmsmT4mlPc')
+
+  return <div>{data && <Booklist data={data} />}</div>
 }
