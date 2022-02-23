5435a3a2ab7085712edd43046c14021c50110347
Archil Jirkvelishvili <archil.j@gmail.com>
2022-02-24 01:48:52 +0400

    feat: fixing fetch

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState, useReducer } from 'react'
-
-// const initState = {
-//   error: null,
-//   loading: false,
-// }
-
-// const reducer = (state, action) => {
-//   // eslint-disable-next-line default-case
-//   switch (action.type) {
-//     case 'loading':
-//       return {...state, loading: true}
-//   }
-// }
+import { useEffect, useState } from 'react'
 
 export function useFetch(url) {
-  // const [state, dispach] = useReducer(reducer, initState)
-
   const [data, setData] = useState(null)
-  // const [loading, setLoading] = useState(false)
-  // const [error, setError] = useState(null)
 
   useEffect(() => {
-    console.log('ha')
     let controller = new AbortController()
     const { signal } = controller
     console.log(controller)
@@ -29,7 +11,6 @@ export function useFetch(url) {
       try {
         const response = await fetch(url, { signal })
         const data = await response.json()
-        console.log(data.items)
         setData(data.items)
       } catch (error) {
         console.log(error.message)
@@ -41,8 +22,6 @@ export function useFetch(url) {
       controller.abort()
     }
   }, [url])
-
-  console.log(2)
 
   return { data }
 }
